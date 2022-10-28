ccd2e1a386387df6ad63a315aeed99d6db50c8bb
Manthri-Mahesh <mantrimahesh999@gmail.com>
2022-10-28 19:34:16 +0530

    created code 5, 6

---
+++ 5. fifth.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    int sum = 10, x = 5;
+    sum += x; // sum = sum + x
+    cout << sum << endl; // 15
+    int fact = 10, y = 5;
+    fact *= y; // fact = fact * y
+    cout << fact << endl; // 50
+    return 0;
+}
