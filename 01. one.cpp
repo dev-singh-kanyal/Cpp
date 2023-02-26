fc693c41b84942c1b55d25ed517cc30335b1cfb4
Dev Singh Kanyal <77870205+dev-singh-kanyal@users.noreply.github.com>
2023-02-26 22:47:24 +0530

    Update 01. one.cpp
    Co-Authored-By: Desika-Vnnghyal <75521120+Desika-Vnnghyal@users.noreply.github.com>

--- 01. one.cpp
+++ 01. one.cpp
@@ -5,8 +5,11 @@ using namespace std;
 int main()
 {
     string name;
+    char job[20];
     cout << "May i know your name: ";
     getline(cin, name);
-    cout << "Welcome Mr " << name << endl;
+    cout << "Your job title: ";
+    cin.get(job, 20);
+    cout << "Welcome " << job << " " << name << endl;
     return 0;
 }
